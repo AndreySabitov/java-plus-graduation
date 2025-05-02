2e2f3ae66adb21b7bee46d70527c2d4f09180429
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-05-02 17:05:40 +0500

    Реализовать Collector

--- stats/collector/src/main/java/ru/practicum/ewm/controller/UserActionController.java
+++ stats/collector/src/main/java/ru/practicum/ewm/controller/UserActionController.java
@@ -2,18 +2,24 @@ package ru.practicum.ewm.controller;
 
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import ru.practicum.ewm.grpc.stats.controller.UserActionControllerGrpc;
 import ru.practicum.ewm.grpc.stats.event.UserActionProto;
+import ru.practicum.ewm.handler.UserActionHandler;
 
 @Slf4j
 @GrpcService
+@RequiredArgsConstructor
 public class UserActionController extends UserActionControllerGrpc.UserActionControllerImplBase {
+    private final UserActionHandler userActionHandler;
 
     @Override
     public void collectUserAction(UserActionProto request, StreamObserver<Empty> responseObserver) {
         log.info("Получили grpc сообщение {}", request);
+
+        userActionHandler.handle(request);
 
         responseObserver.onNext(Empty.getDefaultInstance());
         responseObserver.onCompleted();
